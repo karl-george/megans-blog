e27168e19e4ad009c377654f401efef4934b6520
Karl George <karlgdev@gmail.com>
2023-10-03 15:25:50 +0200

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import AuthProvider from '@/providers/AuthProvider';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
