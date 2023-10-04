f90efb09711e0780b0bf6a2ed93079a726f9acf0
Karl George <karlgdev@gmail.com>
2023-10-04 15:06:35 +0200

    Add get page from search params for pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import BlogList from '@/components/BlogList';
 import CategoryList from '@/components/CategoryList';
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
   return (
     <main className='text-center'>
-      Hello
       <CategoryList />
+      <BlogList page={page} />
     </main>
   );
 }
