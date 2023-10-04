52aeaf0a4049928fd86622d0c79ebadeaa2ecf58
Karl George <karlgdev@gmail.com>
2023-10-04 13:59:10 +0200

    getCategories

---
+++ app/api/categories/route.ts
@@ -0,0 +1,14 @@
+import prisma from '@/utils/connect';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+
+    return new NextResponse(JSON.stringify(categories));
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong' })
+    );
+  }
+};
