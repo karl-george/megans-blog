c38e4074b54e5ef1ef66e5621486f1bd33e851d4
Karl George <karlgdev@gmail.com>
2023-10-04 15:40:08 +0200

    Add singular blog route

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { fetchSinglePost } from '@/utils/fetchSinglePost';
+import { Post } from '@prisma/client';
+import Image from 'next/image';
+
+interface pageProps {}
+
+async function page({ params }: pageProps) {
+  const { slug } = params;
+  const post: Post = await fetchSinglePost(slug);
+
+  return (
+    <div>
+      {post.title}
+      {post.desc}
+      {post?.user?.name}
+      <Image src={post.user.image} width={200} height={200} alt={post.title} />
+    </div>
+  );
+}
+
+export default page;
