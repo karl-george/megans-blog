57a1487997e76b55e72c16c985a1e2003140698b
Karl George <karlgdev@gmail.com>
2023-10-04 14:21:41 +0200

    add categorylist

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import CategoryList from '@/components/CategoryList';
+
 export default function Home() {
-  return <main className='text-center'>Hello</main>;
+  return (
+    <main className='text-center'>
+      Hello
+      <CategoryList />
+    </main>
+  );
 }
