bf9245d03d7defabf6e2dc80177be1d80eed08d0
Karl George <karlgdev@gmail.com>
2023-10-05 18:30:39 +0200

    Add desktop styling to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,22 +7,22 @@ function Header({}: HeaderProps) {
     <div>
       <div className='bg-secondary'>
         <div className='container py-8'>
-          <div className='relative flex flex-col-reverse justify-center items-center space-y-4'>
+          <div className='relative flex flex-col-reverse justify-center items-center space-y-4 lg:items-start'>
             <Image
               src='/hero-img.png'
               alt='hero-img'
-              width={620}
+              width={820}
               height={530}
-              className='object-cover'
+              className='object-cover lg:self-end'
             />
             <div className='absolute text-center'>
-              <div className=''>
-                <h2 className='text-primary hero-text text-[40px] allura bg-white py-1 mb-4 px-4 mx-12'>
+              <div className=' bg-white py-1 mb-4 lg:mb-8 px-4 mx-12'>
+                <h2 className='text-primary hero-text text-[40px] lg:text-8xl allura'>
                   Hi, I'm Megan
                 </h2>
               </div>
-              <div className=''>
-                <h2 className='text-primary hero-text text-[40px] allura bg-white py-1 px-4'>
+              <div className=' bg-white py-1 px-4 lg:ml-24 lg:px-12'>
+                <h2 className='text-primary hero-text text-[40px] lg:text-8xl allura '>
                   Welcome to my blog
                 </h2>
               </div>
