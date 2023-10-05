850645c5eba0b3186b3408fdecd9df032a26de61
Karl George <karlgdev@gmail.com>
2023-10-05 13:04:20 +0200

    Feat: Add authenticated links to navbar

--- components/AuthLinks.tsx
+++ components/AuthLinks.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import MobileMenu from './MobileMenu';
+
 interface AuthLinksProps {}
 
 function AuthLinks({}: AuthLinksProps) {
-  return <div>AuthLinks</div>;
+  const { status } = useSession();
+
+  return (
+    <>
+      {status === 'unauthenticated' ? (
+        <Link href='/sign-in' className='max-lg:hidden lg:block'>
+          Login
+        </Link>
+      ) : (
+        <Link href='/profile' className='max-lg:hidden lg:block'>
+          Profile
+        </Link>
+      )}
+      <MobileMenu status={status} />
+    </>
+  );
 }
 
 export default AuthLinks;
