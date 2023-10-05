afccaa2a438f2a2ef9cc1a579a57db228671d5da
Karl George <karlgdev@gmail.com>
2023-10-05 13:03:59 +0200

    Fix: Signing out and redirect

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,19 +1,16 @@
 'use client';
 
-import { signIn, useSession, signOut } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 
 interface pageProps {}
 
 function page({}: pageProps) {
-  const { data, status } = useSession();
-
-  console.log(data, status);
-
   return (
     <div>
-      <button onClick={() => signIn('google')}>Sign In</button>
-      {/* TODO: Push the user to homepage when signed in */}
-      <button onClick={() => signOut}>Sign Out</button>
+      <button onClick={() => signIn('google', { callbackUrl: '/' })}>
+        Sign In
+      </button>
+      <button onClick={() => signOut()}>Sign out</button>
     </div>
   );
 }
