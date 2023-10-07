b868f9a1db2d6074b51c86d62a93c32ffc221860
Karl George <karlgdev@gmail.com>
2023-10-07 18:03:38 +0200

    Add CategoryCard component

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Category } from '@prisma/client';
+import Image from 'next/image';
 
 interface CategoryCardProps {
   category: Category;
@@ -7,9 +8,22 @@ interface CategoryCardProps {
 
 function CategoryCard({ category }: CategoryCardProps) {
   return (
-    <div>
-      <Link href={`/blog?cat=${category.slug}`}>{category.title}</Link>
-    </div>
+    <Link href={`/blog?cat=${category.slug}`}>
+      <div className='flex flex-col items-center relative'>
+        {category.img && (
+          <Image
+            src={category.img}
+            alt='food-image'
+            width={400}
+            height={300}
+            className='border-[10px] border-secondary'
+          />
+        )}
+        <h3 className='absolute bottom-0 text-2xl uppercase bg-secondary text-primary py-2 px-6'>
+          {category.title}
+        </h3>
+      </div>
+    </Link>
   );
 }
 
