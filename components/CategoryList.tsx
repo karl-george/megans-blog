0079e2c24736348dda803a6367255b334f08c88c
Karl George <karlgdev@gmail.com>
2023-10-07 16:37:02 +0200

    Designing categorylist

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,6 +1,6 @@
-import { fetchCategories } from '@/utils/fetchCategories';
 import { Category } from '@prisma/client';
-import Link from 'next/link';
+import { fetchCategories } from '@/utils/fetchCategories';
+import CategoryCard from './CategoryCard';
 
 interface CategoryListProps {}
 
@@ -8,12 +8,15 @@ async function CategoryList({}: CategoryListProps) {
   const categories: Category[] = await fetchCategories();
 
   return (
-    <div>
-      {categories?.map((category) => (
-        <Link href={`/blog?cat=${category.slug}`} key={category.id}>
-          {category.title}
-        </Link>
-      ))}
+    <div className='container m-16 mx-auto'>
+      <h2 className='allura text-5xl md:text-7xl text-primary mb-12 text-center'>
+        Browse the Categories
+      </h2>
+      <div className='flex flex-col md:flex-row justify-between items-center'>
+        {categories?.map((category) => (
+          <CategoryCard category={category} key={category.id} />
+        ))}
+      </div>
     </div>
   );
 }
