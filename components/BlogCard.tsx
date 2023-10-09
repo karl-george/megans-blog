2af986f06b63acf3f222123941728c3ccebf639a
Karl George <karlgdev@gmail.com>
2023-10-09 17:07:12 +0200

    Design grid for blog posts

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -8,19 +8,18 @@ interface BlogCardProps {
 
 function BlogCard({ post }: BlogCardProps) {
   return (
-    <Link href={`/posts/${post.slug}`} className=''>
-      <div>
+    <Link href={`/posts/${post.slug}`} className='w-[400px]'>
+      <div className='h-[500px] w-[400px] relative'>
         {post.img && (
           <Image
             src={post.img}
             alt='blog poster'
-            width={400}
-            height={540}
+            fill
             className='border-[10px] border-secondary object-cover'
           />
         )}
       </div>
-      <div className='bg-secondary text-primary'>
+      <div className='bg-secondary text-primary space-y-4 text-center py-4'>
         <p className='text-sm text-[#8F8F8F] '>{post.catSlug.toUpperCase()}</p>
         <h3 className='text-3xl'>{post.title}</h3>
         <p className='text-sm text-[#8F8F8F]'>Date</p>
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -20,11 +20,11 @@ async function BlogList({ page, cat }: BlogListProps) {
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
-    <div className='container my-24'>
+    <div className=''>
       <h2 className='allura text-5xl md:text-7xl text-primary mb-8 text-center'>
         My latest thoughts
       </h2>
-      <div className=''>
+      <div className='flex flex-col lg:flex-row w-full gap-x-4 gap-y-8 justify-between'>
         {posts?.map((post) => (
           <BlogCard post={post} key={post.id} />
         ))}
