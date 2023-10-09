8345029026152219ab3e479df1207a4b8ee8e664
Karl George <karlgdev@gmail.com>
2023-10-09 15:33:02 +0200

    Designing blog card

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Post } from '@prisma/client';
 
 interface BlogCardProps {
@@ -7,9 +8,24 @@ interface BlogCardProps {
 
 function BlogCard({ post }: BlogCardProps) {
   return (
-    <div>
-      <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-    </div>
+    <Link href={`/posts/${post.slug}`} className=''>
+      <div>
+        {post.img && (
+          <Image
+            src={post.img}
+            alt='blog poster'
+            width={400}
+            height={540}
+            className='border-[10px] border-secondary object-cover'
+          />
+        )}
+      </div>
+      <div className='bg-secondary text-primary'>
+        <p className='text-sm text-[#8F8F8F] '>{post.catSlug.toUpperCase()}</p>
+        <h3 className='text-3xl'>{post.title}</h3>
+        <p className='text-sm text-[#8F8F8F]'>Date</p>
+      </div>
+    </Link>
   );
 }
 
