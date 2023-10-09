ec852f1d0a4b0d8153459cf31c6615f614e36c57
Karl George <karlgdev@gmail.com>
2023-10-09 15:32:43 +0200

    Adjust post per page

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -8,7 +8,7 @@ export const GET = async (req: NextApiRequest) => {
   const page = parseInt(searchParams.get('page') || '');
   const cat = searchParams.get('cat');
 
-  const POST_PER_PAGE = 2;
+  const POST_PER_PAGE = 6;
 
   const query = {
     take: POST_PER_PAGE,
