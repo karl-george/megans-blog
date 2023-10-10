15b812f6dd800cb99c69113043fedc9cc57b2b7d
Karl George <karlgdev@gmail.com>
2023-10-10 11:53:09 +0200

    Design pagination buttons

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -12,16 +12,18 @@ function Pagination({ page, hasPrev, hasNext }: PaginationProps) {
   const router = useRouter();
 
   return (
-    <div>
+    <div className='my-16 flex flex-row justify-between max-w-lg mx-auto'>
       <button
         onClick={() => router.push(`?page=${page - 1}`)}
         disabled={!hasPrev}
+        className='py-4 px-8 bg-secondary text-primary cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary/50'
       >
         Previous
       </button>
       <button
         onClick={() => router.push(`?page=${page + 1}`)}
         disabled={!hasNext}
+        className='py-4 px-8 bg-secondary text-primary cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary/50'
       >
         Next
       </button>
