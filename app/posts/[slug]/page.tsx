380d4615f55fbfa8e31a9a3d598f3b6b5a7960ac
Karl George <karlgdev@gmail.com>
2023-10-10 12:57:00 +0200

    Style blog articles page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -10,11 +10,18 @@ async function page({ params }: pageProps) {
   const post: Post = await fetchSinglePost(slug);
 
   return (
-    <div>
-      {post.title}
-      {post.desc}
-      {post?.user?.name}
-      <Image src={post.user.image} width={200} height={200} alt={post.title} />
+    <div className='my-12 container flex flex-col justify-center items-center space-y-12 text-primary'>
+      <div className='-mb-6'>{post.catSlug.toUpperCase()}</div>
+      <div className='text-6xl'>{post.title}</div>
+      <div>
+        {post.img && (
+          <Image src={post.img} width={300} height={300} alt={post.title} />
+        )}
+      </div>
+      <div
+        className='max-w-6xl'
+        dangerouslySetInnerHTML={{ __html: post.desc }}
+      />
       <Comments postSlug={slug} />
     </div>
   );
