3a1ee771efbbfceb157f719fc2a06560a2ba8c49
Karl George <karlgdev@gmail.com>
2023-10-10 11:52:40 +0200

    Add filtered by category page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,6 @@
 import BlogList from '@/components/BlogList';
+import CategoryList from '@/components/CategoryList';
+import Header from '@/components/Header';
 
 interface pageProps {}
 
@@ -8,8 +10,11 @@ function page({ searchParams }: pageProps) {
 
   return (
     <div>
-      <h2>{cat} Section</h2>
-      <BlogList page={page} cat={cat} />
+      <Header />
+      <div className='space-y-24 container'>
+        <CategoryList />
+        <BlogList page={page} cat={cat} />
+      </div>
     </div>
   );
 }
