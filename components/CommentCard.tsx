579a920a32cd8e5989860c2f7b19f8df58107209
Karl George <karlgdev@gmail.com>
2023-10-10 16:54:30 +0200

    Add comment card

---
+++ components/CommentCard.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Comment } from '@prisma/client';
+import Image from 'next/image';
+
+interface CommentProps {
+  comment: Comment;
+}
+
+function CommentCard({ comment }: CommentProps) {
+  return (
+    <div className='max-w-[800px] mx-auto mt-24'>
+      <div className='flex gap-4'>
+        <div>
+          <Image
+            src={comment.user.image}
+            width={50}
+            height={50}
+            alt='user-image'
+            className='rounded-full'
+          />
+        </div>
+        <div className='flex flex-col gap-2'>
+          <p className='font-bold'>{comment.user.name}</p>
+          <p className='text-sm'>{comment.createdAt}</p>
+          <p className='text-lg'>{comment.desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CommentCard;
