6c09fa4dfaed9467a1d215a4c35887ecdb3e009c
Karl George <karlgdev@gmail.com>
2023-10-17 15:54:58 +0200

    Add Comment interface

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { formatDateString } from '@/utils/formatDateString';
-import { Comment } from '@prisma/client';
+import { User } from '@prisma/client';
 import Image from 'next/image';
 
 interface CommentProps {
   comment: Comment;
+}
+
+interface Comment {
+  id: string;
+  createdAt: Date;
+  desc: string;
+  userEmail: string;
+  postSlug: string;
+  user: User;
 }
 
 function CommentCard({ comment }: CommentProps) {
@@ -13,16 +22,18 @@ function CommentCard({ comment }: CommentProps) {
     <div className='max-w-[800px] mx-auto mt-24'>
       <div className='flex gap-4'>
         <div>
-          <Image
-            src={comment.user.image}
-            width={50}
-            height={50}
-            alt='user-image'
-            className='rounded-full'
-          />
+          {comment.user.image && (
+            <Image
+              src={comment.user.image}
+              width={50}
+              height={50}
+              alt='user-image'
+              className='rounded-full'
+            />
+          )}
         </div>
         <div className='flex flex-col gap-2'>
-          <p className='font-bold'>{comment.user.name}</p>
+          <p className='font-bold'>{comment?.user?.name}</p>
           <p className='text-sm'>{formatDateString(comment.createdAt)}</p>
           <p className='text-lg'>{comment.desc}</p>
         </div>
